78b726fe5f7a9fe3df8feb008c38a772cc619ce5
Scott Munson <scott@thinkcontra.com>
2013-09-30 18:52:12 -0400

    adds loader.js and t360.js to adotube directory

--- tests/adotube/config.js
+++ tests/adotube/config.js
@@ -1,7 +1,7 @@
 T360_config = {
 	autoplay : false,
-	feedUrl : 'http://tribal360.com/assets/feeder.php?feed=https://buzz60.com/b60-mrss/view/Sam%20Stella%20Zazoom%20Feed/unw3enu5g83fe5s84gle',
-	//feedUrl : '../assets/feeder.php?feed=http://travelbig.com/video-feed/',
+	//feedUrl : '../../assets/feeder.php?feed=https://buzz60.com/b60-mrss/view/Sam%20Stella%20Zazoom%20Feed/unw3enu5g83fe5s84gle',
+	feedUrl : '../../assets/feeder.php?feed=http://travelbig.com/video-feed/',
 	displayAdContainer : 't360_display',
 	videoArea : 't360_video',
 	vpaidFailover : true,
@@ -14,7 +14,7 @@ T360_config = {
 (function(){
 
 	var s = document.createElement('script');
-	s.src = 'http://d2s1vwfhtsw5uw.cloudfront.net/assets/loader.min.js';
+	s.src = 'loader.js';
 	s.async = true;
 	document.getElementsByTagName('head')[0].appendChild(s);
 

6a711bc018f5c7c13c2d9c4437f423ff075d5384
Scott Munson <scott@thinkcontra.com>
2013-10-01 15:12:24 -0400

    updates publisher tests

--- tests/ladolcevitae/config.js
+++ tests/ladolcevitae/config.js
@@ -1,7 +1,6 @@
 T360_config = {
 	autoplay : false,
 	feedUrl : 'http://tribal360.com/assets/feeder.php?feed=https://buzz60.com/b60-mrss/view/Tribal360%20Feed%20Sports/v5ubzz1anfx95makaz',
-	//feedUrl : '../assets/feeder.php?feed=http://travelbig.com/video-feed/',
 	displayAdContainer : 't360_display',
 	videoArea : 't360_video',
 	vpaidFailover : true,

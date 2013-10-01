5c84d7a1fb861bf340fae662fd5751f77bb01885
Scott Munson <scott@thinkcontra.com>
2013-10-01 14:20:34 -0400

    adds ladolcevitae and roadandtrack tests

---
+++ tests/ladolcevitae/config.js
@@ -0,0 +1,20 @@
+T360_config = {
+	autoplay : false,
+	feedUrl : 'http://tribal360.com/assets/feeder.php?feed=https://buzz60.com/b60-mrss/view/Tribal360%20Feed%20Sports/v5ubzz1anfx95makaz',
+	//feedUrl : '../assets/feeder.php?feed=http://travelbig.com/video-feed/',
+	displayAdContainer : 't360_display',
+	videoArea : 't360_video',
+	vpaidFailover : true,
+	openxPixel : '486406',
+	openx : '484145',
+	adapt : 'ladolcevitae',
+};
+
+(function(){
+
+	var s = document.createElement('script');
+	s.src = 'http://d2s1vwfhtsw5uw.cloudfront.net/assets/loader.min.js';
+	s.async = true;
+	document.getElementsByTagName('head')[0].appendChild(s);
+
+})();

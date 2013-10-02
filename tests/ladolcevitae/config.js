8299c8ed97f9e1e5d95d4f87138ac4fc81db3ed9
Scott Munson <scott@thinkcontra.com>
2013-10-02 18:13:32 -0400

    updates some config files and adds /simplejwtest

--- tests/ladolcevitae/config.js
+++ tests/ladolcevitae/config.js
@@ -1,5 +1,5 @@
 T360_config = {
-	autoplay : false,
+	autoplay : true,
 	feedUrl : 'http://tribal360.com/assets/feeder.php?feed=https://buzz60.com/b60-mrss/view/Tribal360%20Feed%20Sports/v5ubzz1anfx95makaz',
 	displayAdContainer : 't360_display',
 	videoArea : 't360_video',

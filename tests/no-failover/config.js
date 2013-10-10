25ae60b674c40286bd6bd3d47180c0fb8e0d52ed
Scott Munson <scott@thinkcontra.com>
2013-10-10 16:35:57 -0400

    add no-failover/ dir for testing Adap/Adotube problem

---
+++ tests/no-failover/config.js
@@ -0,0 +1,21 @@
+T360_config = {
+	autoplay : true,
+	feedUrl : 'http://tribal360.com/assets/feeder.php?feed=https://buzz60.com/b60-mrss/view/Tribal360%20Feed%20Entertainment/unw3enu5g83fe5s84gle',
+	//feedUrl : '../assets/feeder.php?feed=http://travelbig.com/video-feed/',
+	displayAdContainer : 't360_display',
+	videoArea : 't360_video',
+	vpaidFailover : false,
+	openxPixel : '483347',
+	openx : '474222', // PUBLISHER SPECIFIC, OpenX's 'auid' variable
+	adapt : 'adotubeTest', // PUB SPECIFIC, need to figure out what parameter we want to use: 'site'?
+	//customPixel : 'http://ads.blutonic.com/imptr?id=4257&t=2'
+};
+
+(function(){
+
+	var s = document.createElement('script');
+	s.src = 'http://d2s1vwfhtsw5uw.cloudfront.net/assets/loader.min.js';
+	s.async = true;
+	document.getElementsByTagName('head')[0].appendChild(s);
+
+})();

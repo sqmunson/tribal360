cb4f37c5a7d220f0a19234aaf7134f524a0be19a
Scott Munson <scott@thinkcontra.com>
2013-10-16 18:00:51 -0400

    adds cheezburger config to assets

---
+++ assets/pub-configs/cheezburger.js
@@ -0,0 +1,19 @@
+T360_config = {
+	autoplay : false,
+	feedUrl : 'http://tribal360.com/assets/feeder.php?feed=https://buzz60.com/b60-mrss/view/Tribal360%20Feed%20Entertainment/unw3enu5g83fe5s84gle',
+	displayAdContainer : 't360_display',
+	videoArea : 't360_video',
+	vpaidFailover : true,
+	openxPixel : '494269',
+	openx : '494255',
+	adapt : 'failblogcheezburger',
+};
+
+(function(){
+
+	var s = document.createElement('script');
+	s.src = 'http://d2s1vwfhtsw5uw.cloudfront.net/assets/loader.min.js';
+	s.async = true;
+	document.getElementsByTagName('head')[0].appendChild(s);
+
+})();

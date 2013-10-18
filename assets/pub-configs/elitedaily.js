aad87e5f754f5c0bbc6ec9a5d6168f4b83eda6d6
Scott Munson <scott@thinkcontra.com>
2013-10-18 13:57:16 -0400

    updates elitedaily config with new utag

--- assets/pub-configs/elitedaily.js
+++ assets/pub-configs/elitedaily.js
@@ -1,12 +1,14 @@
 T360_config = {
 	autoplay : false,
+	autoplayOnView : true,
 	feedUrl : 'http://tribal360.com/assets/feeder.php?feed=https://buzz60.com/b60-mrss/view/Tribal360%20Feed%20Entertainment/unw3enu5g83fe5s84gle',
 	displayAdContainer : 't360_display',
 	videoArea : 't360_video',
 	vpaidFailover : true,
 	openxPixel : '489953',
 	openx : '489954',
-	adapt : 'elitedaily',
+	adapt : 'H3IEFTyrw3xJm6xrzcLWht70GEaWrG1z',
+	utag : true
 };
 
 (function(){

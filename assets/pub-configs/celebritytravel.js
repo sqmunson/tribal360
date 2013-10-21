9fac0086039981b2dba23778ce58f0cfd4be5d73
Scott Munson <scott@thinkcontra.com>
2013-10-21 14:40:51 -0400

    add celebritychef and celebritytravel configs to assets/

---
+++ assets/pub-configs/celebritytravel.js
@@ -0,0 +1,21 @@
+T360_config = {
+	autoplay : true,
+	autoplayOnView : false,
+	feedUrl : 'http://tribal360.com/assets/feeder.php?feed=https://buzz60.com/b60-mrss/view/Tribal360%20Feed%20Entertainment/unw3enu5g83fe5s84gle',
+	displayAdContainer : 't360_display',
+	videoArea : 't360_video',
+	vpaidFailover : true,
+	openxPixel : '495584',
+	openx : '495583',
+	adapt : 'AiVnje_CA3BJsRMP0_gPXAtRyCRFRZSd',
+	utag : true
+};
+
+(function(){
+
+	var s = document.createElement('script');
+	s.src = 'http://d2s1vwfhtsw5uw.cloudfront.net/assets/loader.min.js';
+	s.async = true;
+	document.getElementsByTagName('head')[0].appendChild(s);
+
+})();

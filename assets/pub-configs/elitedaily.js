bc08fb679eaf05b7fc14e009b1a43be386f2acfd
Scott Munson <scott@thinkcontra.com>
2013-10-21 14:40:03 -0400

    MAJOR: add nocompanion.loader.min.js to assets/ and updates elitedaily config and elitedaily test dir

--- assets/pub-configs/elitedaily.js
+++ assets/pub-configs/elitedaily.js
@@ -2,7 +2,6 @@ T360_config = {
 	autoplay : false,
 	autoplayOnView : true,
 	feedUrl : 'http://tribal360.com/assets/feeder.php?feed=https://buzz60.com/b60-mrss/view/Tribal360%20Feed%20Entertainment/unw3enu5g83fe5s84gle',
-	displayAdContainer : 't360_display',
 	videoArea : 't360_video',
 	vpaidFailover : true,
 	openxPixel : '489953',
@@ -14,7 +13,7 @@ T360_config = {
 (function(){
 
 	var s = document.createElement('script');
-	s.src = 'http://d2s1vwfhtsw5uw.cloudfront.net/assets/loader.min.js';
+	s.src = 'http://d2s1vwfhtsw5uw.cloudfront.net/assets/nocompanion.loader.min.js';
 	s.async = true;
 	document.getElementsByTagName('head')[0].appendChild(s);
 

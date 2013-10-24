07599533a8bf2d4c9fe15ec35ae6509f548f1e24
Scott Munson <scott@thinkcontra.com>
2013-10-24 17:38:14 -0400

    updates all demos with new Adap ID and autoplayOnView

--- demos/blutonic/config.js
+++ demos/blutonic/config.js
@@ -1,12 +1,14 @@
 T360_config = {
-	autoplay : true,
+	autoplay : false,
+	autoplayOnView : true,
 	feedUrl : 'http://tribal360.com/assets/feeder.php?feed=https://buzz60.com/b60-mrss/view/Tribal360%20Feed%20Entertainment/unw3enu5g83fe5s84gle',
 	displayAdContainer : 't360_display',
 	videoArea : 't360_video',
 	vpaidFailover : true,
 	openxPixel : '483347', // PUB SPECIFIC: Tribal360 will provide
 	openx : '474222', // PUB SPECIFIC: Tribal360 will provide
-	adapt : '3', // PUB SPECIFIC: Tribal360 will provide
+	adapt : 'AiVnje_CA3BJsRMP0_gPXAtRyCRFRZSd',
+	utag : true
 	//customPixel : 'http://ads.blutonic.com/imptr?id=4257&t=2'
 };
 
--- demos/trending/config.js
+++ demos/trending/config.js
@@ -1,14 +1,14 @@
 T360_config = {
-	autoplay : true,
+	autoplay : false,
+	autoplayOnView : true,
 	feedUrl : 'http://tribal360.com/assets/feeder.php?feed=https://buzz60.com/b60-mrss/view/Tribal360%20Feed%20Trending/onr2od1q9v5hrlkmwzmz',
-	//feedUrl : '../assets/feeder.php?feed=http://travelbig.com/video-feed/',
 	displayAdContainer : 't360_display',
 	videoArea : 't360_video',
 	vpaidFailover : true,
-	openxPixel : '483347',
-	openx : '474222', // PUBLISHER SPECIFIC, OpenX's 'auid' variable
-	adapt : '3', // PUB SPECIFIC, need to figure out what parameter we want to use: 'site'?
-	//customPixel : 'http://ads.blutonic.com/imptr?id=4257&t=2'
+	openxPixel : '483347', // test pub id
+	openx : '474222', // test pub id
+	adapt : 'AiVnje_CA3BJsRMP0_gPXAtRyCRFRZSd', // RON id, tribal360.com targeted to test ad
+	utag : true
 };
 
 (function(){

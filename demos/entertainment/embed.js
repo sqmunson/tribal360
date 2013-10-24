4f73245381f6a4c88b709e736bbc71e2957081e0
Scott Munson <scott@thinkcontra.com>
2013-10-24 17:36:20 -0400

    deletes sqmunson test and updates txt files with ids and whatnot

--- demos/entertainment/embed.js
+++
@@ -1,6 +0,0 @@
-
-// load t3.js
-document.write('<script src="../../assets/t360.js"></script>');
-
-// load config
-document.write('<script src="config.js"></script>');
--- tests/sqm/embed.js
+++
@@ -1,6 +0,0 @@
-
-// load t3.js
-document.write('<script src="http://tribal360.com/assets/t360.js"></script>');
-
-// load config
-document.write('<script src="http://tribal360.com/assets/pubs/sqmunson.js"></script>');
